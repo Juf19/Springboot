e98f9a680277e0dae7597982b7da1c5bbd6d85b8
Juf19 <jorgeufer.19@gmail.com>
2026-04-01 21:44:17 -0500

    FormsUpStyle

--- gestion-clientes-frontend/src/Componentes/AgregarClientes.jsx
+++ gestion-clientes-frontend/src/Componentes/AgregarClientes.jsx
@@ -5,39 +5,44 @@ const AgregarClientes = () => {
         <div>
             <h1>Agregar Clientes</h1>
             <form>
-                <div class="form-group row">
-                    <label for="colFormLabel" class="col-sm-2 col-form-label">Nombre</label>
-                    <div class="col-sm-10">
-                    <input type="text" class="form-control" id="colFormLabel" />
+                <div className="row">
+
+                    <div className="col-md-4 mb-3">
+                        <label className="form-label">Nombres</label>
+                        <input type="text" className="form-control" placeholder="Nombres" />
+                    </div>
+
+                    <div className="col-md-4 mb-3">
+                        <label className="form-label">Apellidos</label>
+                        <input type="text" className="form-control" placeholder="Apellidos" />
+                    </div>
+
+                    <div className="col-md-4 mb-3">
+                        <label className="form-label">Email</label>
+                        <input type="email" className="form-control" placeholder="email" />
+                    </div>
+
+                    <div className="col-md-4 mb-3">
+                        <label className="form-label">Teléfono</label>
+                        <input type="tel" className="form-control" />
+                    </div>
+                    <div className="col-md-4 mb-3">
+                        <label className="form-label">Dirección</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col-md-4 mb-3">
+                        <label className="form-label">Fecha de Nacimiento</label>
+                        <input type="date" className="form-control" />
                     </div>
                 </div>
-                <div class="form-group row">
-                    <label>Apellido</label>
-                    <input type="text" className="form-control" />
+
+                <div className="row">
+                    <div className="col-12 mt-2">
+                        <button type="submit" className="btn btn-primary">
+                            Guardar Cliente
+                        </button>
+                    </div>
                 </div>
-                <div class="form-group row">
-                    <label>Email</label>
-                    <input type="email" className="form-control" />
-                </div>
-                <div class="form-group row">
-                    <label>Teléfono</label>
-                    <input type="tel" className="form-control" />
-                </div>
-                <div class="form-group row">
-                    <label>Dirección</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div class="form-group row">
-                    <label>Fecha de Nacimiento</label>
-                    <input type="date" className="form-control" />
-                </div>
-                <div class="form-group row">
-                    <label>Fecha de Registro</label>
-                    <input type="date" className="form-control" />
-                </div>
-                <button type="submit" className="btn btn-primary">
-                    Guardar
-                </button>
             </form>
         </div>
     );
