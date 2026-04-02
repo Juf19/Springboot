c72dc72de8272b4fb6bd87b705c31ff96446f316
Juf19 <jorgeufer.19@gmail.com>
2026-04-01 21:17:32 -0500

    FormsupName

--- gestion-clientes-frontend/src/Componentes/AgregarClientes.jsx
+++ gestion-clientes-frontend/src/Componentes/AgregarClientes.jsx
@@ -5,31 +5,33 @@ const AgregarClientes = () => {
         <div>
             <h1>Agregar Clientes</h1>
             <form>
-                <div className="form-group">
-                    <label>Nombre</label>
-                    <input type="text" className="form-control" />
+                <div class="form-group row">
+                    <label for="colFormLabel" class="col-sm-2 col-form-label">Nombre</label>
+                    <div class="col-sm-10">
+                    <input type="text" class="form-control" id="colFormLabel" />
+                    </div>
                 </div>
-                <div className="form-group">
+                <div class="form-group row">
                     <label>Apellido</label>
                     <input type="text" className="form-control" />
                 </div>
-                <div className="form-group">
+                <div class="form-group row">
                     <label>Email</label>
                     <input type="email" className="form-control" />
                 </div>
-                <div className="form-group">
+                <div class="form-group row">
                     <label>Teléfono</label>
                     <input type="tel" className="form-control" />
                 </div>
-                <div className="form-group">
+                <div class="form-group row">
                     <label>Dirección</label>
                     <input type="text" className="form-control" />
                 </div>
-                <div className="form-group">
+                <div class="form-group row">
                     <label>Fecha de Nacimiento</label>
                     <input type="date" className="form-control" />
                 </div>
-                <div className="form-group">
+                <div class="form-group row">
                     <label>Fecha de Registro</label>
                     <input type="date" className="form-control" />
                 </div>
